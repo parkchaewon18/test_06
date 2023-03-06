355f4a7029aff0222fa0c838f948c554c6adb7f8
parkchaewon18 <0101angela@naver.com>
2023-03-06 14:43:37 +0900

    day5_오버라이드

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
         고무오리 a고무오리 = new 고무오리();
         a고무오리.날다();
+        //출력 : 저는 날 수 없어요. ㅜㅜ
     }
 }
 
@@ -25,5 +26,8 @@ class 청둥오리 extends 오리 {
 class 흰오리 extends 오리 {
 }
 class 고무오리 extends 오리 {
+    void 날다() {
+        System.out.println("저는 날 수 없어요. ㅜㅜ");
+    } //오버라이드 : 자신이 가지고 있는 날다를 우선적으로 적용
 }
 
