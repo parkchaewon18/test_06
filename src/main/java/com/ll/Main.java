92911162de11bddcfc69c80f21b82fbb22504f60
parkchaewon18 <0101angela@naver.com>
2023-03-06 14:30:35 +0900

    day1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,13 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        오리 a오리 = new 오리();
+        a오리.날다();
+    }
+}
+
+class 오리 {
+    void 날다() {
+        System.out.println("오리가 날개로 날아갑니다.");
     }
 }
